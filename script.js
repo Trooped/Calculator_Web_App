c908c16c38843d441df556606ba683f2aef83f3f
Trooped <omri.peretz@icloud.noreply.github.com>
2023-12-02 16:37:15 +0200

    Updated logic, working with decimal and correct Math logic and order.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/**
+ * Open:
+ * -add a delete last button
+ * -add a test to check logic before starting, else write ERROR
+ * -style the page :)
+ * -make sure decimals are correct + add a decimal point button + not too many deciamls after the dot
+ */
+
+
+
 function add(first, second){
     return first+second;
 }
@@ -20,16 +30,152 @@ var operator = "";
 
 var result=0;
 
-function operate(first, second, operator){
-    switch(operator){
-        case "add":
-            result = add(first, second);
-        case "subtract":
-            result = subtract(first, second);
-        case "multiply":
-            result = multiply(first, second);
-        case "divide":
-            result = divide(first,second);
+function operate(first, operator, second){
+    first = parseFloat(first);
+    second = parseFloat(second)
 
+    switch(operator){
+        case '+':
+            result = add(first, second);
+            break;
+        case '-':
+            result = subtract(first, second);
+            break;
+        case 'x':
+            result = multiply(first, second);
+            break;
+        case '/':
+            result = divide(first,second);
+            break;
     }
+    console.log(result)
+    return result;
+}
+
+var tempString = ""; //The string containing the button values, and also the result at the end
+//this function updates the screen with the button values on click (also the tempString)
+function updateScreen(buttonText) {
+    if (tempString.length < 13){
+        tempString += buttonText; // Append to temp string
+        document.getElementById("screen").innerText += buttonText; // Append to screen div
+        console.log(tempString);
+    }
+}
+
+document.querySelectorAll('.click-button').forEach(button => {
+    button.addEventListener('click', function() {
+        updateScreen(this.innerText); // Use the text of the clicked button
+    });
+});
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', function() {
+    tempString = "";
+    document.getElementById("screen").innerText = "";
+})
+
+const equals = document.getElementById('equals');
+equals.addEventListener('click', function() {
+    var tempcalc = 0;
+    var result = 0;
+    //add a method that checks if we have the correct nubmer of stuff + correct logic + divisible by 3 etc, e.g 1x2+3/4
+
+    while (tempString.indexOf('x') !== -1 || tempString.indexOf('/') !== -1) {
+        let indexX = tempString.indexOf('x');
+        let indexSlash = tempString.indexOf('/');
+        let operationIndex = -1;
+        let operationString = "";
+    
+        // Determine which operation comes first (if any)
+        if (indexX !== -1 && (indexX < indexSlash || indexSlash === -1)) {
+            operationIndex = indexX;
+        } else if (indexSlash !== -1) {
+            operationIndex = indexSlash;
+        }
+    
+        // If an operation is found, extract and remove the substring
+        if (operationIndex !== -1) {
+            // Adjust the indices to include the surrounding characters
+
+            if (operationIndex > 0) {
+                var startIndex = operationIndex - 1;
+                while (startIndex >= 0 && checkCorrect(tempString.charAt(startIndex))) {
+                    startIndex--;
+                }
+                startIndex++; // Adjust to point to the start of the number
+            }
+            
+            if (operationIndex < tempString.length - 1) {
+                var endIndex = operationIndex + 1;
+                while (endIndex < tempString.length && checkCorrect(tempString.charAt(endIndex))) {
+                    endIndex++;
+                }
+                // endIndex points to the index after the last digit of the second number
+            }
+            
+            // Extract the operation string
+            operationString = tempString.slice(startIndex, endIndex);
+            
+            let firstNum = operationString.slice(0, operationIndex - startIndex);
+            let operator = tempString.charAt(operationIndex);
+            let secondNum = operationString.slice(operationIndex - startIndex + 1);
+            
+
+            let tempResult = operate(firstNum, operator, secondNum);
+            tempResult= parseFloat(tempResult);
+
+            // Replace the operation string in tempString with the result
+            tempString = tempString.slice(0, startIndex) + tempResult.toString() + tempString.slice(endIndex);
+
+
+            result = tempResult.toString(); //may need to change it!!!!!!
+            tempString=result;
+        }
+    }
+//Calculating the + and - logic!
+        while (tempString.indexOf('+') !== -1 || tempString.indexOf('-') !== -1) {
+            let indexPlus = tempString.indexOf('+');
+            let indexMinus = tempString.indexOf('-');
+            let operationIndex = -1;
+        
+            if (indexPlus !== -1 && (indexPlus < indexMinus || indexMinus === -1)) {
+                operationIndex = indexPlus;
+            } else if (indexMinus !== -1) {
+                operationIndex = indexMinus;
+            }
+        
+            if (operationIndex !== -1) {
+                let startIndex = operationIndex;
+                while (startIndex > 0 && checkCorrect(tempString.charAt(startIndex - 1))) {
+                    startIndex--;
+                }
+        
+                let endIndex = operationIndex;
+                while (endIndex < tempString.length - 1 && checkCorrect(tempString.charAt(endIndex + 1))) {
+                    endIndex++;
+                }
+
+                let firstNum = parseFloat(tempString.slice(startIndex, operationIndex));
+                let operator = tempString.charAt(operationIndex);
+                let secondNum = parseFloat(tempString.slice(operationIndex + 1, endIndex + 1));
+        
+                let tempResult = operate(firstNum, operator, secondNum).toString();
+
+                tempString = tempString.slice(0, startIndex) + tempResult.toString() + tempString.slice(endIndex + 1);
+
+                result = tempResult.toString(); // Assuming the operation can result in non-integer values
+            } else {
+                break;
+            }
+        }
+
+            document.getElementById("screen").innerText = result;
+            tempString=result;
+    operationString= "";
+    //tempString = "";
+    tempcalc = 0;
+})
+
+function checkCorrect(cur){
+    return !(cur == '+' || cur=='-'||cur=='x'||cur=='/');
 }
